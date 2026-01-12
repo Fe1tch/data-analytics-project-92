4015251675c8a6c889c2bce86be97703022acc3e
Fe1tch <bezdenezhnykh01@gmail.com>
2026-01-12 16:06:27 +0300

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(*) -- Используем функцию COUNT(*) для подсчёта всех строк
+AS customers_count -- Присваиваем результату псевдоним customers_count с помощью AS
+FROM customers; -- В таблице customers
